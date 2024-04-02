ef199c93027ec02a79f3a9802f189203c72e8e20
tjsiv <tjsivelsjr@gmail.com>
2024-04-01 21:28:04 -0700

    first commit

--- public/index.js
+++ public/index.js
@@ -1,9 +1,14 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+    let response = await fetch('http://localhost:9001/counter/');
+
+    let result = await response.json();
+    console.log(response)
+    console.log(result)
+    let countValue = result.value;
 
     function increment(){
         countValue++;
